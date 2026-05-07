50e9fd8903c9b474cb10f2e2721baaa364c8aaca
Eaint <meemeema1962@gmail.com>
2026-05-07 18:47:01 +0100

    Update NavSys.java

--- OOSD1/java.code/NavSys.java
+++ OOSD1/java.code/NavSys.java
@@ -5,25 +5,42 @@
 package Assignment;
 
 /**
- *
- * @author thein
+ * 
+ * Subclass of StockItem
  */
 public class NavSys extends StockItem {
+    /**
+     * Creating a NavSys item
+     * @param stockCode
+     * @param quantity
+     * @param price 
+     */
     public NavSys (String stockCode, int quantity, double price){
         super(stockCode, quantity, price);
     }
+    
+    /**
+     * Returns the stock name
+     * @return "Navigation System"
+     */
     public String getStockName(){
         return "Navigation System";
     }
+    /**
+     * Returns the stock description
+     * @return "GeoVision Sat Nav"
+     */
     public String getStockDescription(){
         return "GeoVision Sat Nav";
     }
+    /**
+     * Returns full item details
+     * @return 
+     */
     public String toString(){
         return super.toString();
     }
 
-    /**
-     * @param args the command line arguments
-     */
+   
     
 }
